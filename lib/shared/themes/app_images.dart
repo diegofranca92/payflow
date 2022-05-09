4687bf249dfe2b6a47dc99eb5d4db6f565a88fb8
Diego França <diego.franca92@gmail.com>
2022-05-09 18:32:39 -0300

    Tela de Splash e inclusão de imagens

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const logoFull = "assets/img/logofull.png";
+  static const logomini = "assets/img/logomini.png";
+  static const union = "assets/img/union.png";
+  static const person = "assets/img/person.png";
+  static const google = "assets/img/google.png";
+}
